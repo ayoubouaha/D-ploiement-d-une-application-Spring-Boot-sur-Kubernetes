6dfb1778665d83d222495981f5407b307d4b3256
ayoub.ouahaa <ayoub.ouahaa@gmail.com>
2026-01-05 16:54:08 +0100

    Tp realized

---
+++ src/main/java/org/example/tp33/Tp33Application.java
@@ -0,0 +1,13 @@
+package org.example.tp33;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp33Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp33Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/tp33/Tp33ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tp33;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp33ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
